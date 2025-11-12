9d393f3a38a7292a789eec40b1ff9bd52c97a36b
Aaron <33992792+aarondever@users.noreply.github.com>
2025-11-12 22:32:37 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+]
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages,
+)
+
+match sys.argv[2:]:
+    case ["--verbose"]:
+        print(f"User prompt: {response.text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    case _:
+        print(response.text)
