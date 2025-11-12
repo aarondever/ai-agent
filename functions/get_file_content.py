3c10e483cbc06ea8009699b91d95141c681f116d
Aaron <33992792+aarondever@users.noreply.github.com>
2025-11-12 22:33:53 +0800

    functions: get file content

---
+++ functions/get_file_content.py
@@ -0,0 +1,26 @@
+import os
+
+import config
+
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    try:
+        file_abs_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+        if not file_abs_path.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(file_abs_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        with open(file_abs_path, "r") as f:
+            content = f.read(config.MAX_CHARS)
+            if os.path.getsize(file_abs_path) > config.MAX_CHARS:
+                return (
+                    content
+                    + f'[...File "{file_path}" truncated at {config.MAX_CHARS} characters]'
+                )
+            return content
+
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -2,17 +2,22 @@ from functions.get_files_info import get_files_info
 
 
 def test():
+    result = get_files_info("calculator", ".")
     print("Result for current directory:")
-    print(get_files_info("calculator", "."))
+    print(result)
+    print("")
 
+    result = get_files_info("calculator", "pkg")
     print("Result for 'pkg' directory:")
-    print(get_files_info("calculator", "pkg"))
+    print(result)
 
+    result = get_files_info("calculator", "/bin")
     print("Result for '/bin' directory:")
-    print(get_files_info("calculator", "/bin"))
+    print(result)
 
+    result = get_files_info("calculator", "../")
     print("Result for '../' directory:")
-    print(get_files_info("calculator", "../"))
+    print(result)
 
 
 if __name__ == "__main__":
